ac8abfff2cc64ac22427e7135b4107409f86468f
davidfan168 <davidfan168@hotmail.com>
2022-09-21 11:05:49 -0400

    initial commit

--- PlayMode.hpp
+++ PlayMode.hpp
@@ -28,19 +28,18 @@ struct PlayMode : Mode {
 	//local copy of the game scene (so code can change it during gameplay):
 	Scene scene;
 
+	Scene::Transform *player = nullptr;
+	Scene::Transform *ground = nullptr;
+
 	//hexapod leg to wobble:
-	Scene::Transform *hip = nullptr;
-	Scene::Transform *upper_leg = nullptr;
-	Scene::Transform *lower_leg = nullptr;
-	glm::quat hip_base_rotation;
-	glm::quat upper_leg_base_rotation;
-	glm::quat lower_leg_base_rotation;
-	float wobble = 0.0f;
+	// Scene::Transform *hip = nullptr;
+	// Scene::Transform *upper_leg = nullptr;
+	// Scene::Transform *lower_leg = nullptr;
+	// glm::quat hip_base_rotation;
+	// glm::quat upper_leg_base_rotation;
+	// glm::quat lower_leg_base_rotation;
 
-	glm::vec3 get_leg_tip_position();
-
-	//music coming from the tip of the leg (as a demonstration):
-	std::shared_ptr< Sound::PlayingSample > leg_tip_loop;
+	// std::shared_ptr< Sound::PlayingSample > background_music;
 	
 	//camera:
 	Scene::Camera *camera = nullptr;
